67db07e553cb32f2435e51655840e94a05f6abe6
VivanPanda <vivanpanda13@gmail.com>
2025-08-13 14:28:15 +0400

    Change url on config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://vivanpanda.github.io/',
+  url: 'https://arduinomap.me/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
